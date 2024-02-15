cfb998191a914488792ef7f724efab42a754a8a6
Natasha Dudek <62709661+natasha-dudek@users.noreply.github.com>
2024-02-15 16:35:04 -0500

    Refactor (#13)
    * removed unnecessary files
    
    * updated requirements
    
    * removed orphaned functions
    
    * updated text in tutorial

--- ml4microbiome/train.py
+++ ml4microbiome/train.py
@@ -14,47 +14,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score
 from sklearn.model_selection import RandomizedSearchCV
 
-
-def train_model(
-    alg: Literal["random_forest", "lightGBM", "logistic_regression_L1"],
-    X_train: pd.DataFrame,
-    y_train: pd.Series,
-    random_state: int,
-    hyper_parameters: dict[str, Any],
-) -> Any:
-    """Train a model using the specified algorithm and hyperparameters.
-
-    Args:
-        alg: algorithm to use for training.
-            - "random_forest": Random Forest classifier.
-            - "lightGBM": LightGBM classifier.
-            - "logistic_regression_L1": Logistic Regression with L1 regularization.
-        X_train: training feature dataset.
-        y_train: training target labels.
-        random_state: seed for reproducibility.
-        hyper_parameters: hyperparameters for the specified algorithm. The dictionary
-            should contain the hyperparameters as key-value pairs.
-
-    Returns:
-        The trained model.
-
-    Raises:
-        ValueError: if an invalid algorithm name is provided.
-    """
-    if alg == "random_forest":
-        model = RandomForestClassifier(random_state=random_state, **hyper_parameters)
-    elif alg == "lightGBM":
-        model = LGBMClassifier(random_state=random_state, **hyper_parameters)
-    elif alg == "logistic_regression_L1":
-        model = LogisticRegression(random_state=random_state, **hyper_parameters)
-    else:
-        raise ValueError("Invalid algorithm")
-
-    model.fit(X_train.to_numpy(), y_train.to_numpy())
-
-    return model
-
-
 def tune_model(
     alg: Literal["random_forest", "lightGBM", "logistic_regression_L1"],
     param_distributions: dict[str, Any],
